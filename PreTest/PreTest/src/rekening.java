2f1a4b617b6ad95b8d62e1fac05569452bdaa82f
sultanzulvi <akundump27@gmail.com>
2026-04-02 15:21:36 +0700

    pretest

---
+++ PreTest/PreTest/src/rekening.java
@@ -0,0 +1,18 @@
+public class rekening{
+    private String nomorRekening;
+    private int saldo = 0;
+        
+    public int saldoRekening(){
+        return saldo;
+    }
+
+    public void setSaldo(int saldo){
+        if(saldo < -1){
+            System.out.println("Saldo jangan minus!!")
+        }
+        else(
+            System.out.println("Saldo anda : " + saldo)
+        )
+    }
+}
+
